842870ad779a0c680166a7685251905e41075fe3
Akhil M. Vijayan <akhilmv@icloud.com>
2021-04-10 18:26:52 -0500

    New diffusivity UDF added

--- UDF.c
+++ UDF.c
@@ -13,3 +13,20 @@ DEFINE_PROPERTY(evn2,c,t)
 	enerV = thetaV*Ru/MW/(pow(e,(thetaV/temp))-1);
 	return enerV;
 }
+
+DEFINE_DIFFUSIVITY(mun2,c,t,i)
+{
+	real mu;
+	real MW = 28.0134;
+	real CsR;
+	real temp;
+        real A_22 = -7.6303990E-13 ;
+        real B_22 = 1.6878089E-11  ;
+        real C_22 = -1.4004234E-10 ;
+        real D_22 = 2.1427708E-7   ;
+
+        temp  = C_T(c,t);
+	CsR   = D_22*temp*((A_22*log(temp)+B_22)*log(temp)+C_22);
+        mu    = 2.6693E-6*3.14125*pow(MW*temp,0.5)/CsR ;
+	return mu;
+}
