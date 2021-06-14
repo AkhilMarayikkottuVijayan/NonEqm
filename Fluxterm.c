55c526abe3c8d1abe543ec93ac7623ae05632c68
Cephied12 <82404671+Cephied12@users.noreply.github.com>
2021-06-13 23:14:17 -0400

    Add files via upload
    Beginning of Diffusive flux term, still need to determine which indicators are necessary (cell (c) of course...but I need to figure out if the face index (f) is necessary as well) More to come as I continue through the ANSYS UDF manual.

---
+++ Fluxterm.c
@@ -0,0 +1,43 @@
+#include "udf.h"
+
+DEFINE_DFLUX(jn2,c,t,i)
+{
+    real xs;
+    real xr;
+    real Dsr;
+    real Ys;
+    real rho;
+
+
+
+
+
+}
+
+DEFINE_DFLUX(jo2,c,t,i)
+{
+    real xs;
+    real xr;
+    real Dsr;
+    real Ys;
+    real rho;
+
+
+
+
+
+}
+
+DEFINE_DFLUX(jno,c,t,i)
+{
+    real xs;
+    real xr;
+    real Dsr;
+    real Ys;
+    real rho;
+
+
+
+
+
+}
