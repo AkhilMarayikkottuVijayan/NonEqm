f43adb55c715b592ee6f870915dcc508a439a42b
Akhil Marayikkottu Vijayan <akhilm3@login2.stampede2.tacc.utexas.edu>
2021-06-15 18:38:02 -0500

    Reaction modification

--- Interpreted.c
+++ Interpreted.c
@@ -12,7 +12,7 @@ DEFINE_INIT(tv_init,d)
            /* loop over all cells */
                 begin_c_loop_all(c,t)
                 {
-                C_UDMI(c,t,0) = 1200.0;
+                C_UDMI(c,t,0) = C_T(c,t);
                 }
                 end_c_loop_all(c,t)
         }
@@ -38,7 +38,16 @@ DEFINE_ADJUST(tv_comp,d)
                 {
                 enerV = C_UDSI(c,t,0);
                 Tv = thetaV*pow(log((thetaV*Ru/MW/(enerV))+1),-1);
-                C_UDMI(c,t,1) = Tv;
+                C_UDMI(c,t,0) = Tv;
+               /* printf ("Pressure	: %f \n", C_P(c,t));
+                printf ("Temperature    : %f \n", C_T(c,t));
+                printf ("Density        : %f \n", C_R(c,t));
+                printf ("Y0             : %f \n", C_YI(c,t,0));
+                printf ("Y1             : %f \n", C_YI(c,t,1));
+                printf ("Y2             : %f \n", C_YI(c,t,2));
+                printf ("Y3             : %f \n", C_YI(c,t,3));
+                printf ("Y4             : %f \n", C_YI(c,t,4));*/
+                printf ("Vibrational temperature : %f \n", C_UDMI(c,t,0));
                 }
                 end_c_loop (c,t)
         }
@@ -62,5 +71,62 @@ DEFINE_DIFFUSIVITY(mun2,c,t,i)
         CsR   = D_22*pow(temp,((A_22*log(temp)+B_22)*log(temp)+C_22));
         mu    = 2.6693E-6*3.14125*pow(MW*temp,0.5)/CsR ;
         return mu;
+}
+
+/* Vibrational-Translational relaxation source 
+ * terms computed using Landaue-Teller model
+ * is coded up as a source UDF               */
+DEFINE_SOURCE(vt_source,c,t,dS,eqn)
+{
+real    vt,msr,MW,theta;
+real    xr,tau_sr,Asr,temp;
+real    pres,e,C_temp,tau_vs;
+real    density, Ys,tempV;
+real    evsT,evsTV,Ru,NA;
+real    tau_cs,sig_s,C_s,n_s;
+
+e       = 2.718281828   ;
+NA      = 6.0221476E23  ;
+MW      = 28.0134       ;
+theta   = 3.395E+3      ;
+Ru      = 8314.0        ;
+msr     = MW*0.5        ;
+temp    = C_T(c,t)      ;
+pres    = C_P(c,t)      ;
+density = C_R(c,t)      ;
+Ys      = C_YI(c,t,1)   ;
+Asr     = 1.16E-3*pow(msr,0.5)*pow(theta,4/3);
+C_temp  = Asr*(pow(temp,-1/3)-0.015*pow(msr,0.25))-18.42;
+tau_sr  = (1/pres)*pow(e,C_temp);
+
+/* Parks correction */
+
+sig_s   = 1E-20*pow(50000/temp,2);
+C_s     = pow(8*Ru*temp/(3.14125*MW),0.5);
+n_s     = NA*density*Ys/MW;
+tau_cs  = pow(sig_s*C_s*n_s,-1);
+
+/* Since we have only one diatomic species at this 
+ *  * point, we have to just add the two relaxation 
+ *   * time steps to get tau_vs */
+tau_vs  = tau_sr+tau_cs;
+
+tempV   = C_UDMI(c,t,0) ;
+evsT    = theta*Ru/(MW*(pow(e,theta/temp)-1));
+evsTV   = theta*Ru/(MW*(pow(e,theta/tempV)-1));
+
+/*  Landau-Teller formula */
+vt      = -1*density*Ys*(evsT-evsTV)/tau_vs;
+//printf ("VT relaxation source term : %f \n", vt);
+C_UDMI(c,t,1) = vt;
+return vt;
+}
+
+DEFINE_SOURCE(e_vt_source,c,t,dS,eqn)
+{
+real    evt;
+evt     = 0.0;
+//printf ("evt : %f \n", evt);
+return evt;
 }
 
--- Reac.c
+++ Reac.c
@@ -9,12 +9,42 @@ DEFINE_VR_RATE(vol_reac_rate,c,t,r,wk,yk,rate,rr_t)
 
   prod = 1.;
 
-  for(i = 0; i < r->n_reactants; i++)
+/*  for(i = 0; i < r->n_reactants; i++)
     {
        ci     = C_R(c,t) * yk[r->reactant[i]] / wk[r->reactant[i]];
        prod  *= pow(ci, r->exp_reactant[i]);
-    }
-  
+       printf ("yk:  %f \n", yk[r->reactant[i]]);
+       printf ("wk:  %f \n", wk[r->reactant[i]]);
+    }*/
+ 
+   if (!strcmp(r->name, "N2 + N2 <-> 2N + N2"))
+      {
+      /* printf ("Reactant : %f \n ", yk[r->reactant[0]]);
+       printf ("Reaction rate : %f \n", *rate);
+       printf ("Reaction name: %s\n", r->name);
+       printf ("A : %f \n", r->A);
+       printf ("Activation energy : %f \n", r->E);
+       printf ("Pre-exponent : %f \n", r->b);*/
+      } 
+   else if (!strcmp(r->name, "O2 + O2 <-> 2O + O2"))
+      {
+      /* printf ("Reactant : %f \n ", yk[r->reactant[0]]);
+       printf ("Reaction rate : %f \n", *rate);
+       printf ("Reaction name: %s\n", r->name);
+       printf ("A : %f \n", r->A);
+       printf ("Activation energy : %f \n", r->E);
+       printf ("Pre-exponent : %f \n", r->b);*/
+      } 
+   else if (!strcmp(r->name, "O2 + N2 <-> 2O + N2"))
+      {
+      /* printf ("Reactant : %f \n ", yk[r->reactant[0]]);
+ *        printf ("Reaction rate : %f \n", *rate);
+       printf ("Reaction name: %s\n", r->name);
+       printf ("A : %f \n", r->A);
+       printf ("Activation energy : %f \n", r->E);
+       printf ("Pre-exponent : %f \n", r->b);*/
+      } 
+
   /* *rate = r->A * exp( - r->E / (UNIVERSAL_GAS_CONSTANT * C_T(c,t)))*pow(C_T(c,t), r->b) * prod;
   *rr_t = *rate;*/
   /* No "return..;" value. */
