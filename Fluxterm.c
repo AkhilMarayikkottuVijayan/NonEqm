6bbd4d736822df7799671ff39adb70bc8a20cfba
Cephied12 <82404671+Cephied12@users.noreply.github.com>
2021-06-30 02:35:45 -0400

    Update Fluxterm.c

--- Fluxterm.c
+++ Fluxterm.c
@@ -1,43 +1,8 @@
 #include "udf.h"
 
-DEFINE_DFLUX(jn2,c,t,i)
+DEFINE_UDS_FLUX(massflux_plus_diffusiveflux,f,t,i)
 {
-    real xs;
-    real xr;
-    real Dsr;
-    real Ys;
-    real rho;
-
-
-
-
-
-}
-
-DEFINE_DFLUX(jo2,c,t,i)
-{
-    real xs;
-    real xr;
-    real Dsr;
-    real Ys;
-    real rho;
-
-
-
-
-
-}
-
-DEFINE_DFLUX(jno,c,t,i)
-{
-    real xs;
-    real xr;
-    real Dsr;
-    real Ys;
-    real rho;
-
-
-
+    real 
 
 
 }
